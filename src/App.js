efda2b26931fe6aac7e64a393ed99e2fd48152d1
nyx-desdev <nyx.desdev@gmail.com>
2022-04-06 18:25:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const videoRef = useRef(null);
+  const photoRef = useRef(null);
+  const [hasPhoto, setHasPhoto] = useState(false);
+
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: { width: 300, height: 300 },
+      })
+      .then((stream) => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const takePhoto = () => {
+    const width = 300;
+    const height = 300;
+
+    let video = videoRef.current;
+    let photo = photoRef.current;
+
+    photo.width = width;
+    photo.height = height;
+
+    let ctx = photo.getContext('2d')
+    ctx.drawImage(video, 0, 0, width, height)
+    setHasPhoto(true)
+  };
+
+  useEffect(() => {
+    getVideo();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="camera">
+        <video ref={videoRef}></video>
+        <button onClick={takePhoto}>Snap</button>
+      </div>
+      <div className={"result" + (hasPhoto ? "hasPhoto" : "")}>
+        <canvas ref={photoRef}></canvas>
+      </div>
     </div>
   );
 }
