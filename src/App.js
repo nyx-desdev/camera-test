3d6c152965ccbb70cc1162fd8f9e2b33f5afb751
nyx-desdev <nyx.desdev@gmail.com>
2022-04-06 19:25:07 +0530

    inside useeffect

--- src/App.js
+++ src/App.js
@@ -6,72 +6,6 @@ function App() {
   const photoRef = useRef(null);
   const [hasPhoto, setHasPhoto] = useState(false);
 
-  async function getMedia(constraints) {
-    // Older browsers might not implement mediaDevices at all, so we set an empty object first
-    if (navigator.mediaDevices === undefined) {
-      navigator.mediaDevices = {};
-    }
-
-    // Some browsers partially implement mediaDevices. We can't just assign an object
-    // with getUserMedia as it would overwrite existing properties.
-    // Here, we will just add the getUserMedia property if it's missing.
-    if (navigator.mediaDevices.getUserMedia === undefined) {
-      navigator.mediaDevices.getUserMedia = function (constraints) {
-        // First get ahold of the legacy getUserMedia, if present
-        var getUserMedia =
-          navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-        // Some browsers just don't implement it - return a rejected promise with an error
-        // to keep a consistent interface
-        if (!getUserMedia) {
-          return Promise.reject(
-            new Error("getUserMedia is not implemented in this browser")
-          );
-        }
-
-        // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
-        return new Promise(function (resolve, reject) {
-          getUserMedia.call(navigator, constraints, resolve, reject);
-        });
-      };
-    }
-
-    let stream = null;
-
-    try {
-      stream = await navigator.mediaDevices.getUserMedia(constraints);
-      /* use the stream */
-      let video = videoRef.current;
-      video.srcObject = stream;
-      video.play();
-    } catch (err) {
-      /* handle the error */
-      console.log(err);
-    }
-  }
-
-  const getVideo = () => {
-    var constraints = {
-      audio: true,
-      video: { width: 300, height: 300, facingMode: "user" },
-    };
-    getMedia(constraints);
-
-    // console.log('videoref',videoRef)
-    // navigator.mediaDevices
-    //   .getUserMedia({
-    //     video: { width: 300, height: 300 },
-    //   })
-    //   .then((stream) => {
-    //     let video = videoRef.current;
-    //     video.srcObject = stream;
-    //     video.play();
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
-  };
-
   const takePhoto = () => {
     const width = 300;
     const height = 300;
@@ -88,8 +22,55 @@ function App() {
   };
 
   useEffect(() => {
-    getVideo();
-  }, [getVideo]);
+    async function getMedia(constraints) {
+      var constraints = {
+        audio: true,
+        video: { width: 300, height: 300, facingMode: "user" },
+      };
+      // Older browsers might not implement mediaDevices at all, so we set an empty object first
+      if (navigator.mediaDevices === undefined) {
+        navigator.mediaDevices = {};
+      }
+
+      // Some browsers partially implement mediaDevices. We can't just assign an object
+      // with getUserMedia as it would overwrite existing properties.
+      // Here, we will just add the getUserMedia property if it's missing.
+      if (navigator.mediaDevices.getUserMedia === undefined) {
+        navigator.mediaDevices.getUserMedia = function (constraints) {
+          // First get ahold of the legacy getUserMedia, if present
+          var getUserMedia =
+            navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+          // Some browsers just don't implement it - return a rejected promise with an error
+          // to keep a consistent interface
+          if (!getUserMedia) {
+            return Promise.reject(
+              new Error("getUserMedia is not implemented in this browser")
+            );
+          }
+
+          // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+          return new Promise(function (resolve, reject) {
+            getUserMedia.call(navigator, constraints, resolve, reject);
+          });
+        };
+      }
+
+      let stream = null;
+
+      try {
+        stream = await navigator.mediaDevices.getUserMedia(constraints);
+        /* use the stream */
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      } catch (err) {
+        /* handle the error */
+        console.log(err);
+      }
+    }
+    getMedia()
+  }, []);
 
   return (
     <div className="App">
